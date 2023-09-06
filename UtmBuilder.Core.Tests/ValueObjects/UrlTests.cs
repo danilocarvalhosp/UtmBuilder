24d1cbfb933bb6f523f8fca2cb10824cdc5d6a2e
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-09-06 19:53:53 -0300

    Testando por exceções

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core.Tests.ValueObjects
 {
@@ -12,7 +14,21 @@ namespace UtmBuilder.Core.Tests.ValueObjects
         [TestMethod("Deve retornar uma exceção quando a URL for inválida")]
         public void Dado_uma_url_invalida_deve_retornar_uma_excecao()
         {
+            try
+            {
+                var url = new Url("banana");
+                Assert.Fail();
+            }
+            catch (InvalidUrlException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
 
+        [TestMethod("Não deve retornar uma exceção quando a URL for válida")]
+        public void Dado_uma_url_valida_nao_deve_retornar_uma_excecao()
+        {
+            Assert.Fail();
         }
     }
 }
