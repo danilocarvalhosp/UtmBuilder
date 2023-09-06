3d347fdd1e3b3cf4b3e4607dbda80462149fac4d
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-09-06 19:42:19 -0300

    Organizando os testes de unidade

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class UrlTests
+    {
+        [TestMethod]
+        public void X()
+        {
+
+        }
+    }
+}
