aea7859b48329b729ba3f4f3c2d3b010aa9e8a20
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-09-06 16:30:11 -0300

    Source Generators

--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,8 +11,6 @@ namespace UtmBuilder.Core.ValueObjects
 {
     public class Url : ValueObject
     {
-        private const string UrlRegexPattern = @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
-
         /// <summary>
         /// Create a new URL
         /// </summary>
@@ -20,10 +19,7 @@ namespace UtmBuilder.Core.ValueObjects
         public Url(string address)
         {
             Address = address;
-            if(Regex.IsMatch(Address, UrlRegexPattern))
-            {
-                throw new InvalidUrlException();
-            }
+            InvalidUrlException.ThrowIfInvalid(address);
         }
 
         /// <summary>
