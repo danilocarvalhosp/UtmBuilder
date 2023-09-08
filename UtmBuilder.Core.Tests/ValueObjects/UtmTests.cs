5c556b84d5a2f2e15cedb2582500457f61e5dcf9
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-09-08 18:43:00 -0300

    Testando os Implicit Operators

--- UtmBuilder.Core.Tests/ValueObjects/UtmTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UtmTests.cs
@@ -10,21 +10,37 @@ namespace UtmBuilder.Core.Tests.ValueObjects
     [TestClass]
     public class UtmTests
     {
+        private const string Result = "https://balta.io/" +
+                                        "?utm_source=src" +
+                                        "&utm_medium=med" +
+                                        "&utm_campaign=name" +
+                                        "&utm_id=id" +
+                                        "&utm_term=term" +
+                                        "&utm_content=cnt";
+        private readonly Url Url = new("https://balta.io/");
+        private readonly Campaign Campaign = new(
+            "src", "med", "name", "id", "term", "cnt");
+
         [TestMethod]
         public void ShoudReturnUrlFromUtm()
         {
-            var url = new Url("https://balta.io/");
-            var campaign = new Campaign("src", "med", "name", "id", "term", "cnt");
-            var utm = new Utm(url, campaign);
+            var utm = new Utm(Url, Campaign);
 
-            var result = "https://balta.io/" + 
-                                            "?utm_source=src" +
-                                            "&utm_medium=med" +
-                                            "&utm_campaign=name" +
-                                            "&utm_id=id" +
-                                            "&utm_term=term" +
-                                            "&utm_content=cnt";
-            Assert.AreEqual(result, utm.ToString());
+            Assert.AreEqual(Result, utm.ToString());
+            Assert.AreEqual(Result, (string)utm);
+        }
+
+        [TestMethod]
+        public void ShoudReturnUtmFromUrl()
+        {
+            Utm utm = Result;
+            Assert.AreEqual("https://balta.io/", utm.Url.Address);
+            Assert.AreEqual("src", utm.Campaign.Source);
+            Assert.AreEqual("med", utm.Campaign.Medium);
+            Assert.AreEqual("name", utm.Campaign.Name);
+            Assert.AreEqual("id", utm.Campaign.Id);
+            Assert.AreEqual("term", utm.Campaign.Term);
+            Assert.AreEqual("cnt", utm.Campaign.Content);
         }
     }
 }
